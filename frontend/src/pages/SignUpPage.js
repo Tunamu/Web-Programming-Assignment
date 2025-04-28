be6389ca32ae134022a567f55987a083f501e116
Tunamu <tunamuswork@gmail.com>
2025-04-28 19:57:22 +0200

    added signup and login frontend

--- frontend/src/pages/SignUpPage.js
+++ frontend/src/pages/SignUpPage.js
@@ -1,5 +1,5 @@
 import './SignUpPage.css'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function SignUpPage() {
     const [UserInfo, setUserInfo] = useState({
@@ -9,11 +9,69 @@ function SignUpPage() {
         password_confirmation: "",
     });
 
+    const [isSignUpPart, setIsSignUpPart] = useState(true);
+
+    useEffect(() => {
+        if(isSignUpPart){
+
+        }
+    }, [isSignUpPart]);
+
+    function handleLogin(e){
+        console.log(e)
+    }
+
+    function handleSignUp(e){
+        console.log(e)
+    }
+
     return (
-        <div>
-            SignUp Page
+        <div className={"SignUpPage"}>
+            <div className={"Button-Section"}>
+                <button onClick={() => setIsSignUpPart(true)}>SignUp</button>
+                <button onClick={() => setIsSignUpPart(false)}>Login</button>
+            </div>
+
+            {isSignUpPart ? (
+                <>
+                    <h2>SignUp</h2>
+                    <input
+                        placeholder={"Username"}
+                        onChange={(e) => setUserInfo({...UserInfo, username: e.target.value})}
+                    />
+                    <input
+                        placeholder={"Email"}
+                        onChange={(e) => setUserInfo({...UserInfo, email: e.target.value})}
+                    />
+                    <input
+                        type="password"
+                        placeholder={"Password"}
+                        onChange={(e) => setUserInfo({...UserInfo, password: e.target.value})}
+                    />
+                    <input
+                        type="password"
+                        placeholder={"Confirm Password"}
+                        onChange={(e) => setUserInfo({...UserInfo, password_confirmation: e.target.value})}
+                    />
+                    <button onClick={handleSignUp} className={"Confirm-Button"}>SignUp</button>
+                </>
+            ) : (
+                <>
+                    <h2>Login</h2>
+                    <input
+                        placeholder={"username"}
+                        onChange={(e) => setUserInfo({...UserInfo, username: e.target.value})}
+                    />
+                    <input
+                        type="password"
+                        placeholder={"Password"}
+                        onChange={(e) => setUserInfo({...UserInfo, password: e.target.value})}
+                    />
+                    <button onClick={handleLogin} className={"Confirm-Button"}>Login</button>
+                </>
+            )}
         </div>
-    )
+        )
 }
 
 export default SignUpPage;
