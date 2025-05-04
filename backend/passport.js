97a1465565c20085c26915ba3a8de672da7d346b
Tunamu <tunamuswork@gmail.com>
2025-05-04 02:52:28 +0200

    bugfix

--- backend/passport.js
+++ backend/passport.js
@@ -1,15 +1,14 @@
 import passportGithub from "passport-github2"
 import passport from "passport";
 const GithubStrategy = passportGithub.Strategy;
-
-const GITHUB_CLIENT_ID = "Ov23liEu6uW2IVkI2GEb";
-const GITHUB_CLIENT_SECRET = "04a59e9ac1bd81592edb50575dd06ac4d1d4c40a";
+import dotenv from "dotenv";
+dotenv.config();
 
 passport.use(
     new GithubStrategy(
         {
-            clientID: GITHUB_CLIENT_ID,
-            clientSecret: GITHUB_CLIENT_SECRET,
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
             callbackURL: "/api/Auth/github/callback",
         },
         function (accessToken, refreshToken, profile, done) {
--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,9 +1,9 @@
 import express from "express";
 import passport from "passport";
 const router = express.Router();
+import dotenv from "dotenv";
 
-const CLIENT_LOGOUT_URL = "http://localhost:3000/";
-const CLIENT_LOGIN_URL="http://localhost:3000/SignUp";
+dotenv.config();
 
 router.get("/login/success", (req, res) => {
     if (req.user) {
@@ -30,7 +30,7 @@ router.get("/logout", (req, res) => {
 
         req.session.destroy(() => {
             res.clearCookie("connect.sid"); // Express-session cookie’sini temizle
-            res.redirect(CLIENT_LOGOUT_URL); // Örn: http://localhost:3000/
+            res.redirect(process.env.CLIENT_LOGOUT_URL); // Örn: http://localhost:3000/
         });
     });
 });
@@ -41,7 +41,7 @@ router.get("/github", passport.authenticate("github", { scope: ["profile"] }));
 router.get(
     "/github/callback",
     passport.authenticate("github", {
-        successRedirect: CLIENT_LOGIN_URL,
+        successRedirect: process.env.CLIENT_LOGIN_URL,
         failureRedirect: "/login/failed",
     })
 );
